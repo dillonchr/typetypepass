c259d2853540227ef5fcad54166f149e4c0347b1
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-01-30 15:38:30 -0600

    made app a little more dynamically deployable

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux';
 import io from 'socket.io-client';
 import identity from './identity';
 
-const socket = io(`https://${window.location.hostname}:7715`);
+const socket = io(`https://${window.location.hostname}:${window.location.port}`);
 
 const initialState = {
     name: identity.getPlayerName(),
