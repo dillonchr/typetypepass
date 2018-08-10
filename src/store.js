dfafe4e0baa3c71cdcb907e6d44f5ced8a89882e
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-08-10 20:24:26 +0000

    secured again

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 import identity from './identity';
 import { getCurrentUrl } from 'preact-router';
 
-const socket = io('http://api.typetypepass.com/');
+const socket = io('https://api.typetypepass.com/');
 
 const initialState = {
     name: identity.getPlayerName(),
